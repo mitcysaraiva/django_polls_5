9e65c0e6e27007b0444d275c24bdfc2151539686
MITCY <saraivamitcy@gmail.com>
2023-09-06 08:55:44 -0300

    Paginação simples com ListView.

--- polls/views.py
+++ polls/views.py
@@ -2,7 +2,7 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from polls.models import Question
 from django.views.generic import DetailView, ListView, TemplateView
-
+from django.contrib import messages
 
 from django.shortcuts import render
 
@@ -74,7 +74,11 @@ class QuestionUpdateView(UpdateView):
 class QuestionDeleteView(DeleteView):
     model = Question
     template_name = 'polls/question_confirm_delete_form.html'
-    success_url = reverse_lazy('polls_list')
+    success_url = reverse_lazy('polls_all')
+    success_message = 'Pergunta excluída com sucesso.'
+    def form_valid(self, request, *args, **kwargs):
+        messages.success(self.request, self.success_message)
+        return super(QuestionDeleteView, self).form_valid(request, *args, **kwargs)
 class QuestionDetailView(DetailView):
     model = Question
     template_name = 'polls/question_detail.html'
@@ -88,3 +92,4 @@ class QuestionListView(ListView):
 
 class SobreTemplateView(TemplateView):
     template_name = 'polls/sobre.html'
+
