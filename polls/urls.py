c98b0ef834c97c45e2b280b3110964a3f7363af0
mitcy saraiva <saraivamitcy@gmail.com>
2023-11-07 11:12:37 -0300

    votação na enquete

--- polls/urls.py
+++ polls/urls.py
@@ -34,6 +34,10 @@ urlpatterns = [
     path('alternativa/<int:pk>/edit', views.ChoiceUpdateView.as_view(), name="choice_edit"), 
     path('alternativa/<int:pk>/delete', views. ChoiceDeleteView.as_view(), name="choice_delete"),
 
+    path('pergunta/<int:question_id>/vote',
+         views.vote,
+         name="poll_vote"
+    )
 ]
 
 
--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,12 @@
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse_lazy
 from polls.models import Question
 from django.views.generic import DetailView, ListView, TemplateView
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Sum
 
 
 from django.shortcuts import render, get_object_or_404
@@ -58,7 +60,7 @@ class QuestionCreateView(LoginRequiredMixin, CreateView):
     template_name = 'polls/question_form.html'
     fields = ('question_text', 'pub_date', )
     success_url = reverse_lazy('polls_list')
-    
+
     def get_context_data(self, **kwargs):
         context = super(QuestionCreateView, self).get_context_data(**kwargs)
         context['form_title'] = 'Criando uma pergunta'
@@ -100,6 +102,13 @@ class QuestionDetailView(DetailView):
     model = Question
     template_name = 'polls/question_detail.html'
     context_object_name = 'question'
+
+    def get_context_data(self, **kwargs):
+        context = super(QuestionDetailView, self).get_context_data(**kwargs)
+        votes = Choice.objects.filter(question=context['question']).aggregate(total=Sum('votes')) or 0
+        context['total_votes'] = votes.get('total')
+
+        return context
 
 class QuestionListView(ListView):
     model = Question
@@ -171,3 +180,19 @@ class ChoiceDeleteView(LoginRequiredMixin, DeleteView):
     def get_success_url(self, *args, **kwargs): 
         question_id = self.object.question.id 
         return reverse_lazy('poll_edit', kwargs={'pk': question_id})
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    if request.method == 'POST':
+        try:
+            selected_choice = question.choice_set.get(pk=request.POST["choice"])
+        except (KeyError, Choice.DoesNotExist):
+            messages.error(request, 'Selecione uma alternativa para votar')
+        else:
+            selected_choice.votes += 1
+            selected_choice.save()
+            messages.success(request, 'Seu voto foi registrado com sucesso')
+            return redirect(reverse_lazy("poll_show", args=(question.id,)))
+    
+    context = {'question': question}
+    return render(request, 'polls/question_detail.html', context)
