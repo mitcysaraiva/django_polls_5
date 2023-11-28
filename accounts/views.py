4a20e1e6be16853e0f5b41870cd88391e5906764
mitcy saraiva <saraivamitcy@gmail.com>
2023-11-28 10:08:54 -0300

    Lista de Enquetes Votadas

--- accounts/views.py
+++ accounts/views.py
@@ -6,6 +6,9 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.contrib.auth.hashers import make_password # para criptografar a senha
 from django.contrib import messages
+from django.views.generic import TemplateView
+from polls.models import QuestionUser
+
 
 from django.contrib.auth import get_user_model
 User = get_user_model() # obtém o model padrão para usuários do Django
@@ -39,3 +42,13 @@ class AccountUpdateView(LoginRequiredMixin, UpdateView):
     def form_valid(self, form): # executa quando os dados estiverem válidos
         messages.success(self.request, self.success_message)
         return super(AccountUpdateView, self).form_valid(form)
+
+class AccountTemplateView(LoginRequiredMixin, TemplateView):
+    template_name = 'accounts/user_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(AccountTemplateView, self).get_context_data(**kwargs)
+        voted = QuestionUser.objects.filter(user=self.request.user)
+        context['questions_voted'] = voted
+
+        return context
